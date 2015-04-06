f8f2ba2c53eb1c1be3bcde774e644485311f4b83
Nathan Perkins <nathan@maxmo.net>
2015-04-06 12:44:08 -0400

    Optimize RX global.

--- RX_global.m
+++ RX_global.m
@@ -7,21 +7,26 @@ img_size2=size(Data,2);
 channels=size(Data,3);
 
 % Separate block into color channels to calculate K matrix
-CC=reshape(Data,[],channels);
-K=cov(CC);
+CC = reshape(Data,[],channels);
+Kinv = inv(cov(CC));
 
 % Create mean color column vector
-mu=mean(CC)';
+mu = mean(CC);
+
+% zero-mean
+CCrows = size(CC, 1);
+CC = CC - (ones(CCrows, 1) * mu);
 
 % Preallocate
-img_out = zeros(img_size1, img_size2);
+img_out = zeros(CCrows, 1);
 
-for i=1:img_size1
-    for j=1:img_size2
-        % Locate center pixel and convert to column vector
-        r = squeeze(Data(i, j, :));
+for i = 1:CCrows
+    % Locate center pixel and convert to column vector
+    r = CC(i, :);
 
-        % Run RX detector on center pixel
-        img_out(i,j)=(r-mu)'/K*(r-mu);
-    end
+    % Run RX detector on center pixel
+    img_out(i)= r * Kinv * r';
 end
+
+img_out = reshape(img_out, img_size1, img_size2);
+
